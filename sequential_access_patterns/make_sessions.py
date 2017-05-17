5a8127f6cbd4a15e92976d72acb085e114766114
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2017-05-17 13:54:45 +0200

    worked on skript to identify sessions

---
+++ sequential_access_patterns/make_sessions.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+#This script reads data from the database and writes file with sessions
+#Each of these sessions is represented by a line in the file.
+#
+#Additionally, there is some preprocessing being done: Namely, all accesses that
+#occur less than a second after the last one are removed as likely not human generated.
+#
+
+import datetime as dt
+import functools as ft
+
+#Session timeout constant
+SESSION_TIMEOUT_SECS = 10
+filters = [
+        lambda s: "SiteAssets" not in s,
+        lambda s: "_catalogs" not in s,
+        lambda s: not s.endswith(".css") and not s.endswith(".js"),
+        lambda s: "Preview Images" not in s,
+        lambda s: not s.endswith("SitePages"),
+        lambda s: not s.endswith("GWSGroupsList"),
+        lambda s: not s.endswith("Announcements"),
+        lambda s: not s.endswith("LA_Assignments"),
+        lambda s: not s.endswith("Forms"),
+        lambda s: not s.endswith("StructuredMaterials")
+]
+#Model of action
+class Action:
+    def __init__(self,url,time,event):
+        self.time = time
+        self.event = event
+        self.url = url
+
+
+#reads the file accesses.csv, parsing dates into objects
+byuser = dict()
+for line in open("./accesses.csv"):
+    parts = line.split(",")
+    if(parts[0] == "user"):
+        continue
+    if not parts[0] in byuser:
+        byuser[parts[0]] = list()
+
+    date_format = "%Y-%m-%dT%H:%M:%S"
+    time = dt.datetime.strptime(parts[1],date_format)
+    event = parts[2]
+    #Remove commas from urls. also remove trailing whitespace
+    url = ft.reduce(lambda a,b: a+b,parts[3:]).translate({",":None}).replace('"','').strip()
+    if(all(fil(url) for fil in filters)):
+        byuser[parts[0]].append(Action(url,time,event))
+
+#This function is used to split sessions. It generally splits a into sublists.
+#A split is made whenever fun returns true for list elements i and i+1
+def split_lis_at(lis,fun):
+    res = list()
+    prevsplit = 0
+    for i, item in enumerate(lis):
+        is_at_end = i == (len(lis) -1)
+        should_split = is_at_end or fun(lis[i],lis[i + 1]) 
+        if(should_split):
+            res.append(lis[prevsplit:i])
+            prevsplit = i + 1
+    return res
+sessions = list()
+for actions in byuser.values():
+    actions.sort(key=lambda action:action.time)
+    this_user_sessions = split_lis_at(actions, lambda a,b: (b.time-a.time).seconds > SESSION_TIMEOUT_SECS)
+    sessions.extend(this_user_sessions)
+
+
+#Filter out where two requests happen at the same time
+sessions_only_human_accesses = [[action for (i,action) in enumerate(session) if (i == len(session) - 1) or not session[i].time == session[i+1].time] for session in sessions]
+
+resultfile = open('sessions.csv','w')
+for session in sessions:
+    if len(session) > 1:
+        resultfile.write(",".join([str(action.url) for action in session]) + '\n')
+
+#Some statistics on number of session and click length
+#print("numusers" + str(len(byuser.keys())))
+#print("numsessions" + str(len(sessions)))
+#print("avglensessions" + str(sum(len(session) for session in sessions) / len(sessions)))
+#print the average time between requests for each session
+#for session in sessions_only_human_accesses:
+    #times = list()
+    #for i, action in enumerate(session):
+        #if not i == (len(session)-1):
+            #times.append((session[i+1].time - action.time).seconds)
+    #if(len(session) > 1):
+        #print(sum(times) / len(times))
+
