6621e8a749b9ee21c277b456e9043f80a3cb053a
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2017-05-31 13:16:09 +0200

    add rudimentary tooltips

--- timespent/src/index.js
+++ timespent/src/index.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 import test_data from './test_data.json'
+import './style.css'
 
 
 
@@ -14,10 +15,20 @@ const pie = d3.pie().value(d => d.seconds_spent)
 const slices = pie(test_data)
 const arc = d3.arc().innerRadius(30).outerRadius(50)
 const color = d3.scaleOrdinal(d3.schemeCategory10)
+const ttdiv = d3.select('body').append('div').attr('id','tooltip').style('opacity',0)
 
 g
   .selectAll('path.slice').data(slices).enter().append('path')
   .attr('class','slice')
   .attr('d',arc)
   .attr('fill',d => color(d.data.name))
+  .on('mouseover', d => {
+    ttdiv.transition().duration(200).style('opacity',.9)    
+    ttdiv.html(d.data.name)
+      .style('left',d3.event.pageX + 'px')
+      .style('top',(d3.event.pageY -28) + 'px')
+  })
+  .on('mouseout',d => {
+    ttdiv.transition().duration(200).style('opacity',0)    
+  })
 
