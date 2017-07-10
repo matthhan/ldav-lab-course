a1bf98cbb51cf587d33bb59d7a4ce748b7df3246
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2017-07-10 14:20:08 +0200

    clean up sequential access patterns folder

--- do_everything.sh
+++ do_everything.sh
@@ -2,5 +2,9 @@
 
 mkdir base_data;
 cd data_extraction;
-./download_everything.py $LDAVLAB_UN $LDAVLAB_PW;
+#./download_everything.py $LDAVLAB_UN $LDAVLAB_PW;
+cd ..;
+
+cd sequential_access_patterns;
+./do_everything.sh;
 cd ..;
--- sequential_access_patterns/time_differences.R
+++
@@ -1,2 +0,0 @@
-data <- read.csv("res.csv")
-data %>% count(X0)  %>% ggplot() + aes(X0,n) + geom_line() + scale_y_log10() + xlim(0,100)
