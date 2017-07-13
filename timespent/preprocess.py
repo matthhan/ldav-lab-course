96f5b8ae75cf48304ce19d71d1224d31a772cb5b
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2017-07-13 11:42:18 +0200

    timespent but better

--- timespent/preprocess.py
+++ timespent/preprocess.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import os
+import copy
 import datetime as dt
 import functools as ft
 import itertools as it
@@ -8,7 +10,7 @@ import sys
 import copy
 import json
 import urllib.parse
-
+inpfilename= sys.argv[1]
 #filters applied to urls before processing
 is_not_a_form = lambda s: not re.search(r"/StructuredMaterials/Forms",s)
 is_not_layout = lambda s: not re.search(r"/_layouts/",s)
@@ -38,6 +40,13 @@ def url_is_interesting(url):
     return all(fil(url) for fil in filters)
 def extract_method(line):
     return line.split(",")[2]
+def extract_userid(line):
+    return line.split(',')[1]
+def extract_datetime(line):
+    date_format = "%Y-%m-%d %H:%M:%S.%f"
+    parts = line.split(',')
+    return dt.datetime.strptime(parts[1] if '.' in parts[1] else parts[1] + '.0',date_format)
+                                
 
 def parse_path_items(path):
     if(path==""):
@@ -80,17 +89,30 @@ def categorize_request(url):
         return ["Wiki"]
     return None
 
+class Request: 
+    def get_rest_categories(self):
+        cp = copy.deepcopy(self)
+        if(cp.category):
+            cp.category = cp.category[1:]
+        return cp
+
+
 by_course = dict()
-atfirstline = True
-for line in open(sys.argv[1]):
-    if(atfirstline):
-        atfirstline = False
+by_user = dict()
+file_lines = os.stat(inpfilename).st_size / 200
+i = 0
+for line in open(inpfilename):
+    i+= 1
+    if(i == 1):
         continue
+    if(i % 1000 == 0):
+        print(str(((i/file_lines)*100)//1) + "% Done", end= '\r', file=sys.stderr)
 
     url = extract_url(line)
     if(not url):
         continue
     method = extract_method(line)
+    
     if(not (method == 'GET')):
         continue
     if(not url_is_interesting(url)):
@@ -99,17 +121,45 @@ for line in open(sys.argv[1]):
     course = extract_course(url)
     if(course not in by_course):
         by_course[course] = list()
-    category = categorize_request(url)
-    if(category):
-        by_course[course].append(category)
+    res = Request()
+    res.category = categorize_request(url)
+    res.time_spent = 1
+    res.datetime = extract_datetime(line)
+    res.userid = extract_userid(line)
+    if(res.userid not in by_user):
+        by_user[res.userid] = list()
 
-def summarize_accesses(accesses):
-    names = list(set([lis[0] for lis in accesses if lis]))
+
+    if(res.category):
+        by_course[course].append(res)
+        by_user[res.userid].append(res)
+default_time = 600
+def add_time_spent(one_users_requests):
+    one_users_requests.sort(key=lambda request: request.datetime)
+    for i, request in enumerate(one_users_requests):
+        is_last_request = i == (len(one_users_requests)-1)
+        if(is_last_request):
+            request.time_spent = default_time
+        else:
+            next_request = one_users_requests[i+1]
+            request.time_spent = calculate_time_spent(request,next_request)
+
+def calculate_time_spent(request,next_request):
+    passed_time = (next_request.datetime - request.datetime).seconds
+    two_hours = 7200 
+    if(passed_time > two_hours):
+        return default_time
+    else:
+        return passed_time
+
+
+def summarize_accesses(requests):
+    first_level_names = list(set([request.category[0] for request in requests if request.category]))
     
     res = list()
-    for name in names:
-        time_spent = len([x for x in accesses if x and x[0] == name ])
-        children = summarize_accesses([x[1:] for x in accesses if x and x[0] == name ])
+    for name in first_level_names:
+        time_spent = sum([request.time_spent for request in requests if request.category and request.category[0] == name ])
+        children = summarize_accesses([request.get_rest_categories() for request in requests if request.category and request.category[0] == name ])
         res.append({
             'name':urllib.parse.unquote(name),
             'seconds_spent':time_spent,
@@ -117,6 +167,8 @@ def summarize_accesses(accesses):
         })
     return res
 
+for user,requests in by_user.items():
+    add_time_spent(requests)
 res = dict()
 for course, accesses in by_course.items():
     res[course] = summarize_accesses(accesses)
